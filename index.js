64c8c44c3351ad67e1a7bf4aa7f01a4bc1c68b2f
Brody Chen <rufengc@zenefits.com>
2021-01-12 22:01:56 -0800

    Add new product page

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ mongoose
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({});
   res.render('products/index', { products });
+});
+
+app.post('/products', async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+app.get('/products/new', async (req, res) => {
+  res.render('products/new');
 });
 
 app.get('/products/:id', async (req, res) => {
