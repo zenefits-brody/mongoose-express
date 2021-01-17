c899e68e96b5ae967ab47e63b76359c481f7f2f3
Brody Chen <rufengc@zenefits.com>
2021-01-16 20:17:01 -0800

    Add edit page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const Product = require('./models/product');
 
@@ -17,7 +18,10 @@ mongoose
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({});
@@ -38,6 +42,18 @@ app.get('/products/:id', async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render('products/show', { product });
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', { product });
+});
+
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+  res.redirect(`/products/${id}`);
 });
 
 app.listen(3000, () => {
